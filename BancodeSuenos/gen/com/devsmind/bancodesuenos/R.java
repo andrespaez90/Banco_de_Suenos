21b6cbbe9e409a23dbbee20fc6a6de6be82322b0
Romel Palomino <romelpalomino@hotmail.com>
2013-12-12 16:40:41 -0500

    log in facebook

--- BancodeSuenos/gen/com/devsmind/bancodesuenos/R.java
+++ BancodeSuenos/gen/com/devsmind/bancodesuenos/R.java
@@ -359,18 +359,19 @@ containing a value of this type.
         public static final int photos=0x7f0a0004;
     }
     public static final class string {
-        public static final int action_forgot_password=0x7f070034;
-        public static final int action_settings=0x7f07001b;
-        public static final int action_sign_in_register=0x7f070032;
-        public static final int action_sign_in_short=0x7f070033;
-        public static final int app_id=0x7f07001a;
+        public static final int action_forgot_password=0x7f07003b;
+        public static final int action_settings=0x7f070022;
+        public static final int action_sign_in_register=0x7f070039;
+        public static final int action_sign_in_short=0x7f07003a;
+        public static final int app_id=0x7f070020;
         public static final int app_name=0x7f070019;
-        public static final int boton_cancelar=0x7f07002e;
-        public static final int boton_confirmar=0x7f07002d;
-        public static final int boton_login=0x7f07002a;
-        public static final int boton_ok=0x7f070027;
-        public static final int boton_omitir=0x7f070028;
-        public static final int boton_siguiente=0x7f07002b;
+        public static final int boton_cancelar=0x7f070035;
+        public static final int boton_confirmar=0x7f070034;
+        public static final int boton_login=0x7f070031;
+        public static final int boton_ok=0x7f07002e;
+        public static final int boton_omitir=0x7f07002f;
+        public static final int boton_siguiente=0x7f070032;
+        public static final int cancelled=0x7f07001a;
         public static final int com_facebook_choose_friends=0x7f07000f;
         public static final int com_facebook_dialogloginactivity_ok_button=0x7f070000;
         public static final int com_facebook_internet_permission_error_message=0x7f070013;
@@ -396,29 +397,35 @@ containing a value of this type.
         public static final int com_facebook_usersettingsfragment_log_in_button=0x7f070008;
         public static final int com_facebook_usersettingsfragment_logged_in=0x7f070009;
         public static final int com_facebook_usersettingsfragment_not_logged_in=0x7f07000a;
-        public static final int error_field_required=0x7f070039;
-        public static final int error_incorrect_password=0x7f070038;
-        public static final int error_invalid_email=0x7f070036;
-        public static final int error_invalid_password=0x7f070037;
-        public static final int hello_world=0x7f07001c;
-        public static final int login_progress_signing_in=0x7f070035;
-        public static final int pregunta_como_ahorras=0x7f070029;
-        public static final int pregunta_costo=0x7f070022;
-        public static final int pregunta_cuando=0x7f070021;
-        public static final int pregunta_nombre=0x7f070025;
-        public static final int pregunta_sueno=0x7f070020;
+        public static final int error=0x7f07001f;
+        public static final int error_field_required=0x7f070040;
+        public static final int error_incorrect_password=0x7f07003f;
+        public static final int error_invalid_email=0x7f07003d;
+        public static final int error_invalid_password=0x7f07003e;
+        public static final int exception=0x7f070021;
+        public static final int hello_user=0x7f07001d;
+        public static final int hello_world=0x7f070023;
+        public static final int login_progress_signing_in=0x7f07003c;
+        public static final int ok=0x7f07001c;
+        public static final int permission_not_granted=0x7f07001b;
+        public static final int pregunta_como_ahorras=0x7f070030;
+        public static final int pregunta_costo=0x7f070029;
+        public static final int pregunta_cuando=0x7f070028;
+        public static final int pregunta_nombre=0x7f07002c;
+        public static final int pregunta_sueno=0x7f070027;
         /**  Strings related to login 
          */
-        public static final int prompt_email=0x7f070030;
-        public static final int prompt_password=0x7f070031;
-        public static final int signo_pesos=0x7f070023;
-        public static final int texto_explicacionNombre=0x7f070026;
-        public static final int title_activity_custom_goal=0x7f070024;
-        public static final int title_activity_login=0x7f07002f;
-        public static final int title_activity_main=0x7f07001e;
-        public static final int title_activity_new_goal=0x7f07001d;
-        public static final int title_activity_photos=0x7f07002c;
-        public static final int titulo_nuevoSueno=0x7f07001f;
+        public static final int prompt_email=0x7f070037;
+        public static final int prompt_password=0x7f070038;
+        public static final int signo_pesos=0x7f07002a;
+        public static final int success=0x7f07001e;
+        public static final int texto_explicacionNombre=0x7f07002d;
+        public static final int title_activity_custom_goal=0x7f07002b;
+        public static final int title_activity_login=0x7f070036;
+        public static final int title_activity_main=0x7f070025;
+        public static final int title_activity_new_goal=0x7f070024;
+        public static final int title_activity_photos=0x7f070033;
+        public static final int titulo_nuevoSueno=0x7f070026;
     }
     public static final class style {
         /** 
--- BancodeSuenos/src/com/devsmind/bancodesuenos/StartActivity.java
+++ BancodeSuenos/src/com/devsmind/bancodesuenos/StartActivity.java
@@ -3,21 +3,34 @@ package com.devsmind.bancodesuenos;
 import com.bd.persistencia.PersistManager;
 import com.db.Activities.LoginActivity;
 import com.db.Activities.NewGoalActivity;
+import com.db.Activities.NewGoalActivity;
+import com.facebook.AppEventsLogger;
+import com.facebook.FacebookAuthorizationException;
+import com.facebook.FacebookOperationCanceledException;
 import com.facebook.Session;
+import com.facebook.SessionState;
+import com.facebook.UiLifecycleHelper;
 import com.facebook.model.GraphUser;
+import com.facebook.widget.FacebookDialog;
 import com.facebook.widget.LoginButton;
+import com.facebook.widget.ProfilePictureView;
 
 import android.location.Address;
 import android.os.Bundle;
 import android.app.Activity;
+import android.app.AlertDialog;
 import android.app.Fragment;
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.drawable.BitmapDrawable;
 import android.support.v4.app.FragmentActivity;
+import android.util.Log;
 import android.view.Menu;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.TextView;
 
 public class StartActivity extends FragmentActivity implements OnClickListener {
  
@@ -25,8 +38,44 @@ public class StartActivity extends FragmentActivity implements OnClickListener {
 	private ImageView Logo;
 	private Button IngresarCorreo;
 	//Facebook
-    private LoginButton LoginFacebook;
-    private GraphUser user;
+	private LoginButton LoginFacebook;
+    private ProfilePictureView profilePictureView;
+    private PendingAction pendingAction = PendingAction.NONE;    
+    private ImageView perfil;
+    private final String PENDING_ACTION_BUNDLE_KEY = "com.facebook.samples.hellofacebook:PendingAction";
+    //Facebook bd
+    private GraphUser usuario;
+    private String cumple;
+    private String nombre;
+    private String apellido;
+    private Bitmap  fotoPerfil;
+    
+
+    private enum PendingAction {
+        NONE,
+        POST_PHOTO,
+        POST_STATUS_UPDATE
+    }
+    private UiLifecycleHelper uiHelper;
+
+    private Session.StatusCallback callback = new Session.StatusCallback() {
+        @Override
+        public void call(Session session, SessionState state, Exception exception) {
+            onSessionStateChange(session, state, exception);
+        }
+    };
+
+    private FacebookDialog.Callback dialogCallback = new FacebookDialog.Callback() {
+        @Override
+        public void onError(FacebookDialog.PendingCall pendingCall, Exception error, Bundle data) {
+            Log.d("HelloFacebook", String.format("Error: %s", error.toString()));
+        }
+
+        @Override
+        public void onComplete(FacebookDialog.PendingCall pendingCall, Bundle data) {
+            Log.d("HelloFacebook", "Success!");
+        }
+    };
 	
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -34,6 +83,26 @@ public class StartActivity extends FragmentActivity implements OnClickListener {
         setContentView(R.layout.activity_start);
         Init();
         addListeners();
+        
+        //facebook
+        uiHelper = new UiLifecycleHelper(this, callback);
+        uiHelper.onCreate(savedInstanceState);
+
+        if (savedInstanceState != null) {
+            String name = savedInstanceState.getString(PENDING_ACTION_BUNDLE_KEY);
+            pendingAction = PendingAction.valueOf(name);
+        }
+
+        LoginFacebook.setUserInfoChangedCallback(new LoginButton.UserInfoChangedCallback() {
+            @Override
+            public void onUserInfoFetched(GraphUser user) {
+                StartActivity.this.usuario = user;
+                updateUI();
+                handlePendingAction();
+                Intent i=new Intent(getApplicationContext(),NewGoalActivity.class);
+            }
+        });
+
     }
 
     private void Init() {
@@ -48,14 +117,7 @@ public class StartActivity extends FragmentActivity implements OnClickListener {
 		Logo.setOnClickListener(this);
 		IngresarCorreo.setOnClickListener(this);
 		LoginFacebook.setOnClickListener(this);
-        LoginFacebook.setUserInfoChangedCallback(new LoginButton.UserInfoChangedCallback() {
-            @Override
-            public void onUserInfoFetched(GraphUser user) {
-                StartActivity.this.user = user;
-                // It's possible that we were waiting for this.user to be populated in order to post a
-                // status update.
-            }
-        });
+        
 	}
     
    
@@ -96,5 +158,83 @@ public class StartActivity extends FragmentActivity implements OnClickListener {
     	}
 		
 	}
+	
+	//Comienzan funciones facebook
+	
+	@Override
+    protected void onResume() {
+        super.onResume();
+        uiHelper.onResume();
+
+        // Call the 'activateApp' method to log an app event for use in analytics and advertising reporting.  Do so in
+        // the onResume methods of the primary Activities that an app may be launched into.
+        AppEventsLogger.activateApp(this);
+
+        updateUI();
+    }
+
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+        uiHelper.onSaveInstanceState(outState);
+
+        outState.putString(PENDING_ACTION_BUNDLE_KEY, pendingAction.name());
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        uiHelper.onActivityResult(requestCode, resultCode, data, dialogCallback);
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+        uiHelper.onPause();
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        uiHelper.onDestroy();
+    }
+
+    private void onSessionStateChange(Session session, SessionState state, Exception exception) {
+        if (pendingAction != PendingAction.NONE &&
+                (exception instanceof FacebookOperationCanceledException ||
+                exception instanceof FacebookAuthorizationException)) {
+                new AlertDialog.Builder(StartActivity.this)
+                    .setTitle(R.string.cancelled)
+                    .setMessage(R.string.permission_not_granted)
+                    .setPositiveButton(R.string.ok, null)
+                    .show();
+            pendingAction = PendingAction.NONE;
+        } else if (state == SessionState.OPENED_TOKEN_UPDATED) {
+            handlePendingAction();
+        }
+        updateUI();
+    }
+
+    private void updateUI() {
+        Session session = Session.getActiveSession();
+        boolean enableButtons = (session != null && session.isOpened());
+
+        if (enableButtons && usuario != null) {
+            profilePictureView.setProfileId(usuario.getId());
+            nombre=usuario.getFirstName();
+            ImageView fbImage = ( ( ImageView)profilePictureView.getChildAt( 0));
+            Bitmap    bitmap  = ( ( BitmapDrawable) fbImage.getDrawable()).getBitmap();
+            perfil.setImageBitmap(bitmap);
+        } else {
+            profilePictureView.setProfileId(null);
+            greeting.setText(null);
+        }
+    }
+
+    private void handlePendingAction() {
+        pendingAction = PendingAction.NONE;
+
+        
+    }
     
 }
